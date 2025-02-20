c9743f7de0f5c92562b20af22b343b39b09836a2
Israel Akpadja <drimmaculate147@gmail.com>
2025-02-20 15:36:35 +0000

    fixed classes not recognized error

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -5,16 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Course;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class CourseController extends Controller
 {
+    use AuthorizesRequests;
     public function store(Request $request)
     {
         $this->authorize('create', Course::class);
 
         $course = Course::create([
             'title' => $request->title,
-            'description' => $request->description,
+            'description' => $request->description, 
             'duration' => $request->duration,
             'start_date' => $request->start_date,
             'end_date' => $request->end_date,
